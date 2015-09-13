9c05c899a4a6273201e3b27798bab9f1a6c79d16
mefernandez <mariano.fernandez@gmail.com>
2015-09-13 08:35:05 +0200

    Issue #4, bootstrap jumbotron template

--- modules/temply-core/index.js
+++ modules/temply-core/index.js
@@ -3,10 +3,10 @@
 var fs = require('fs');
 
 exports.render = function (filePath, options, callback) { // define the template engine
-  fs.readFile(filePath, function (err, content) {
+  fs.readFile(filePath, function (err, buffer) {
     if (err) return callback(new Error(err));
     // this is an extremely simple template engine
-    var rendered = 'Delivered by Temply!!!';
+    var rendered = buffer.toString();
     return callback(null, rendered);
   })
 }  
--- modules/temply-core/test/index-test.js
+++ modules/temply-core/test/index-test.js
@@ -19,12 +19,14 @@ describe('Temply', function() {
   });
 
   it('should render a template file as a string', function(done) {
-    var filePath = path.join(__dirname, 'template.html');
+    var filePath = path.join(__dirname, 'template-01.html');
     var options = {};
     temply.render(filePath, options, function (err, rendered) {
       expect(err).to.be.null;
       expect(rendered).to.be.a('string');
+      expect(rendered).to.equal('Found template-01.html');
       done();
     });
   });
+
 });
