c6d85a7eb0d61bc452016241d9fb4c3e727504a7
mefernandez <mariano.fernandez@gmail.com>
2015-10-23 14:45:11 +0200

    Issue #6, saving changes to static content in template

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var log = require('debug')('index.js');
 
 router.get('/', function(req, res, next) {
   res.render('jumbotron/index.html');
@@ -11,6 +12,26 @@ router.get('/control', function(req, res, next) {
 
 router.get('/author', function(req, res, next) {
   res.render('jumbotron-author/index.html');
+});
+
+router.post('/api/edit', function(req, res, next) {
+  var fs = require('fs');
+  var readFileOptions = {
+    encoding: 'utf8',
+    flag: 'r'
+  };
+  var path = require('path');
+  var filePath = path.join(__dirname, '../template/jumbotron-author/index.html');
+  var html = fs.readFileSync(filePath, readFileOptions);
+  var cheerio = require('cheerio');
+  var $ = cheerio.load(html);
+  var data = req.body;
+  var id = '#' + data.id;
+  var text = data.text;
+  var $el = $(id);
+  $el.text(text);
+  fs.writeFileSync(filePath, $.html());
+  res.send(204);
 });
 
 module.exports = router;
--- template/jumbotron-author/main.js
+++ template/jumbotron-author/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+  var my_selector_generator = new CssSelectorGenerator;
+
+  $('[contentEditable]').on('blur', function(ev) {
+    var element = ev.target;
+    var $target = $(element);
+    var id = $target.attr('id');
+    var text = $target.text();
+    var change = {
+      id: id,
+      text: text
+    };
+    $.post('/api/edit', change, null, 'json');
+  })
  
   $("#ala-carousel").owlCarousel(
     {
@@ -19,7 +33,7 @@ $(document).ready(function() {
       content: "This mode allows you to edit this page's content."
     },
     {
-      element: "#main-title",
+      element: "#tour-main-title",
       placement: "top",
       title: "Edit this title",
       content: "Click on this title to edit"
