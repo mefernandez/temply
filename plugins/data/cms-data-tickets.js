a986dc58f01772353f5e955dc34c2f68ab1ee425
mefernandez <mariano.fernandez@gmail.com>
2015-12-14 13:38:48 +0100

    Issue #4, cmd-data-tickets

--- plugins/data/cms-data-tickets.js
+++ plugins/data/cms-data-tickets.js
@@ -3,36 +3,36 @@
 var request = require('superagent');
 var parseString = require('xml2js').parseString;
 var _ = require('lodash');
-var log = require('debug')('temply:cms-data-rss-feed');
+var log = require('debug')('temply:cms-data-tickets');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/tricket');
+
 
 /**
- * Get A List Apart's latest articles
+ * List tickets using MongoDb
+ * @see http://mongoosejs.com/docs/index.html
  */
 module.exports = function(data, $element, callback) {
-  var url = '/api/tickets';
-  log('Reading tickets from URL: ' + url);
-  request
-    .get(url)
-    .set('User-Agent', 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)')
-    .set('Accept', 'text/xml; charset=UTF-8')
-    .end(function(err, res) {
-      if (err) throw new Error(err);
-      log('Reading feed from URL: ' + url);
-      var xml = res.text;
-      parseString(xml, function (err, result) {
-        var articles = _.chain(result.rss.channel[0].item)
-          .take(5)
-          .map(function(item) {
-            return {
-              title: !!item.title ? item.title[0] : '',
-              link: !!item.link ? item.link[0] : '',
-              description: !!item.description ? item.description[0] : '', 
-              author: !!item.author ? item.author[0] : ''
-            };
-          })
-          .value();
-        callback(articles);
-      });
-    });
+  var data = {
+    "contents": [
+      {
+        "_id": "asdf234798",
+        "customer": "Customer",
+        "ticket_id": "CUS-1",
+        "status": "Open",
+        "title": "New Ticket",
+        "worklog": 12,
+        "dateCreated": "2015-12-14T12:12:47.901Z"
+      }
+    ],
+    "page": {
+      "current": 1,
+      "size": 10,
+      "total": 999
+    }
+};
+
+callback(data);
 
 }
